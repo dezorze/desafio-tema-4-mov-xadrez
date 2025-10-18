76a5259f5dd541665fab4efb2a258634ea41fa23
dezorze <lfdprogram@gmail.com>
2025-10-17 22:51:40 -0300

    add o movimento de mais 3 pecas, conforme o nivel novato

--- desafio-xadrez.c
+++ desafio-xadrez.c
@@ -5,10 +5,31 @@ int main() {
     int movimento = 1;
     // esse while(movimento--) é intessante: enquanto movimento for true (1) ele decrementa.
     while(movimento--) {
+        printf("Movimento do Cavalo:\n");
         for(int i = 0; i < 2; i++) {
             printf("Baixo\n");
         }
         printf("Esquerda\n");
     }
+
+    // Os proximos fors simulam os movimentos das outras pecas.
+    printf("\nMovimento da Torre:\n");
+    for(int i = 0; i < 5; i++) {
+        printf("Direita\n");
+    }
+
+    printf("\nMovimento do Bispo:\n");
+    for(int i = 0; i < 5; i++){
+        printf("Cima\n");
+        printf("Direita\n");
+
+    }
+
+    printf("\nMovimento da Rainha:\n");
+    for(int i = 0; i < 8; i++){
+        printf("Esquerda\n");
+
+    }
+
     return 0;
 }
