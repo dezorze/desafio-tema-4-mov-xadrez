a9c3034352f58d584f03688a019bfc95a251a4e4
dezorze <lfdprogram@gmail.com>
2025-10-19 13:08:35 -0300

    todos os codigos descomentados, incluindo recursividade

--- desafio-xadrez.c
+++ desafio-xadrez.c
@@ -53,35 +53,35 @@ void cavalo(int num) {
 
 
 int main() {
-    // //nesse codigo, um for dentro de um while reproduz o mov do cavalo
-    // int movimento = 1;
-    // // esse while(movimento--) é intessante: enquanto movimento for true (1) ele decrementa.
-    // while(movimento--) {
-    //     printf("Movimento do Cavalo:\n");
-    //     for(int i = 0; i < 2; i++) {
-    //         printf("Baixo\n");
-    //     }
-    //     printf("Esquerda\n");
-    // }
+    //nesse codigo, um for dentro de um while reproduz o mov do cavalo
+    int movimento = 1;
+    // esse while(movimento--) é intessante: enquanto movimento for true (1) ele decrementa.
+    while(movimento--) {
+        printf("Movimento do Cavalo:\n");
+        for(int i = 0; i < 2; i++) {
+            printf("Baixo\n");
+        }
+        printf("Esquerda\n");
+    }
 
-    // // Os proximos fors simulam os movimentos das outras pecas.
-    // printf("\nMovimento da Torre:\n");
-    // for(int i = 0; i < 5; i++) {
-    //     printf("Direita\n");
-    // }
+    // Os proximos fors simulam os movimentos das outras pecas.
+    printf("\nMovimento da Torre:\n");
+    for(int i = 0; i < 5; i++) {
+        printf("Direita\n");
+    }
 
-    // printf("\nMovimento do Bispo:\n");
-    // for(int i = 0; i < 5; i++){
-    //     printf("Cima\n");
-    //     printf("Direita\n");
+    printf("\nMovimento do Bispo:\n");
+    for(int i = 0; i < 5; i++){
+        printf("Cima\n");
+        printf("Direita\n");
 
-    // }
+    }
 
-    // printf("\nMovimento da Rainha:\n");
-    // for(int i = 0; i < 8; i++){
-    //     printf("Esquerda\n");
+    printf("\nMovimento da Rainha:\n");
+    for(int i = 0; i < 5; i++){
+        printf("Esquerda\n");
 
-    // }
+    }
 
     //Usando recursividade para simular o movimento das pecas:
     int n = 5;
